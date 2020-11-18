79567ce870433051263823f6501054f1f4d9b432
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-11-18 08:05:40 -0800

    explicit args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh -l
 
 ALPINE_VERSION=$1
+BRANCH=$2
+TOKEN=$3
 
-cd /docker-action
+cd /docker-action || exit 1
 echo "creating docker image with alpine version: $ALPINE_VERSION"
 
 # here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
-docker build -t docker-action --build-arg alpine_version="$ALPINE_VERSION" . && docker run docker-action
+docker build . -t docker-action \
+    --build-arg alpine_version="$ALPINE_VERSION" \
+    --build-arg branch="$BRANCH" \
+    --build-arg token="$TOKEN" \
+    && docker run docker-action
 
 
